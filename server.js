7ef0cef80c2defce96e89f11e960c6931ca3144e
1nfrarouge <samantha.yc.lim@gmail.com>
2024-01-16 03:31:27 -0500

    Add dynamic navigation in header

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+var passport = require('passport');
 var methodOverride = require('method-override');
 
 require('dotenv').config();
+require('./config/database');
+require('./config/passport');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/');
 
 var app = express();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,15 +25,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(session({
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
+
+app.use(methodOverride('_method'));
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/app', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
