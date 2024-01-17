320ec08267ea9f6ac4a54d08597423dccc482cf4
1nfrarouge <samantha.yc.lim@gmail.com>
2024-01-17 12:54:59 -0500

    Finish new reminder

--- controllers/reminders.js
+++ controllers/reminders.js
@@ -2,10 +2,15 @@ const Reminder = require('../models/reminder');
 
 
 module.exports = {
-    index
+    index,
+    new: newReminder
 };
 
 async function index(req, res) {
     const reminders = await Reminder.find({user: req.user._id});
     res.render('reminders/index', { reminders });
+}
+
+function newReminder(req, res) {
+    res.render('reminders/new');
 }
--- routes/reminders.js
+++ routes/reminders.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const remindersCtrl = require('../controllers/reminders');
 
+
 // All paths start with /reminders
 
 // GET /reminders  (index action/functionality)
 router.get('/', remindersCtrl.index);
+// GET /reminders/new (new action)
+router.get('/new', remindersCtrl.new);
 
 module.exports = router;
