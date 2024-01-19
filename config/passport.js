39cae8f3c1c6062d00e4c09bf49d94f49c419d1a
1nfrarouge <samantha.yc.lim@gmail.com>
2024-01-19 03:07:05 -0500

    Code Clean Up

--- config/passport.js
+++ config/passport.js
@@ -3,18 +3,15 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/user');
 
 passport.use(new GoogleStrategy(
-  // Configuration object
+
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK
   },
-  // The verify callback function...
-  // Marking a function as an async function allows us
-  // to consume promises using the await keyword
+
   async function(accessToken, refreshToken, profile, cb) {
-    // When using async/await  we use a
-    // try/catch block to handle an error
+ 
     try {
       // A user has logged in with OAuth...
       let user = await User.findOne({ googleId: profile.id });
@@ -39,6 +36,5 @@ passport.serializeUser(function(user, cb) {
 });
 
 passport.deserializeUser(async function(userId, cb) {
-  // It's nice to be able to use await in-line!
   cb(null, await User.findById(userId));
 });
